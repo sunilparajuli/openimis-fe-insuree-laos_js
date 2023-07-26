c23bdab3cab77b9872dce54dfd01041eae6c9964
wzglinieckisoldevelo <wzgliniecki@soldevelo.com>
2023-07-26 09:19:35 +0200

    Revert "ONI-121: Add support for secondary calendar display in the insuree fields. (#68)"
    This reverts commit 40dac654df0010be9cf5300e1d16a191e4655809.

--- src/components/InsureeSearcher.js
+++ src/components/InsureeSearcher.js
@@ -172,7 +172,7 @@ class InsureeSearcher extends Component {
       ),
       (insuree) => insuree.email,
       (insuree) => insuree.phone,
-      (insuree) => formatDateFromISO(this.props.modulesManager, this.props.intl, insuree.dob, this.props.isSecondaryCalendarEnabled),
+      (insuree) => formatDateFromISO(this.props.modulesManager, this.props.intl, insuree.dob),
     ];
     for (var i = 0; i < this.locationLevels; i++) {
       // need a fixed variable to refer to as parentLocation argument
@@ -182,9 +182,9 @@ class InsureeSearcher extends Component {
       );
     }
     formatters.push(
-      (insuree) => formatDateFromISO(this.props.modulesManager, this.props.intl, insuree.dob, this.props.isSecondaryCalendarEnabled),
+      (insuree) => formatDateFromISO(this.props.modulesManager, this.props.intl, insuree.validityFrom),
       filters.showHistory &&
-      ((insuree) => formatDateFromISO(this.props.modulesManager, this.props.intl, insuree.dob, this.props.isSecondaryCalendarEnabled)),
+      ((insuree) => formatDateFromISO(this.props.modulesManager, this.props.intl, insuree.validityTo)),
       (insuree) => (
         <Grid container wrap="nowrap" spacing="2">
           <Grid item>
@@ -300,7 +300,6 @@ const mapStateToProps = (state) => ({
   submittingMutation: state.insuree.submittingMutation,
   mutation: state.insuree.mutation,
   confirmed: state.core.confirmed,
-  isSecondaryCalendarEnabled: state.core.isSecondaryCalendarEnabled ?? false,
 });
 
 const mapDispatchToProps = (dispatch) => {
